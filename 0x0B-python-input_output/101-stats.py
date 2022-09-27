8de5005f45556aeee38bf574f43db9040dc67f31
NaheemahBello <nimabello009@gmail.com>
2022-09-27 12:18:38 -0700

    101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,6 +1,8 @@
-#!/usr/bin/python3ads from standard input and computes metrics.
+#!/usr/bin/python3
+# 101-stats.py
+# Naheemah Bello
+"""Reads from standard input and computes metrics.
 
-"""
 After every ten lines or the input of a keyboard interruption (CTRL + C),
 prints the following statistics:
     - Total file size up to that point.
